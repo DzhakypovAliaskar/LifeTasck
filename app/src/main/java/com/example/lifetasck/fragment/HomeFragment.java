c7f496204c7fe97c35eb25a8732e36488415ca01
DzhakypovAliaskar <dzhakypov007@gmail.com>
2021-11-14 17:37:37 +0200

    1дз переделал

--- app/src/main/java/com/example/lifetasck/fragment/HomeFragment.java
+++ app/src/main/java/com/example/lifetasck/fragment/HomeFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
@@ -14,11 +15,13 @@ import android.view.ViewGroup;
 import com.example.lifetasck.R;
 import com.example.lifetasck.databinding.FragmentCreateTaskBinding;
 import com.example.lifetasck.databinding.FragmentHomeBinding;
+import com.example.lifetasck.utils.Constants;
 
 import org.jetbrains.annotations.NotNull;
 
 public class HomeFragment extends Fragment {
     FragmentHomeBinding binding;
+    String userTask;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,11 +33,15 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment);
+        if (getArguments() != null){
+            userTask = (String) getArguments().getSerializable(Constants.USER_TASK);
+        }
         binding.addFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Navigation.findNavController(requireView()).navigate(R.id.createTaskFragment);
             }
         });
-    }
+         }
 }
